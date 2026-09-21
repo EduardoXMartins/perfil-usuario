220dc5726489aacf805c7fbf9186023af16a892f
EduardoXMartins <ximeneseduardo128@gmail.com>
2026-09-21 20:37:46 -0300

    atividade frontend

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import PerfilUsuario from './componentes/PerfilUsuario.vue';
+</script>
+
+<template>
+  <PerfilUsuario nome="João" periodo="Primeiro semestre" curso="matematica"></PerfilUsuario>
+  <PerfilUsuario nome="Fernando" periodo="Segundo semestre" curso="matematica"></PerfilUsuario>
+  <PerfilUsuario nome="Julia" periodo="Quinto semestre" curso="matematica"></PerfilUsuario>
+
+</template>
+
+<style scoped></style>
---
+++ src/componentes/PerfilUsuario.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { defineProps } from 'vue';
+defineProps(["nome", "periodo", "curso"])
+
+</script>
+
+<template>
+    <div class="caixa">
+        <h2>{{ nome }}</h2>
+        <p>{{ curso }}</p>
+        <small>{{ periodo }}</small>
+    </div>
+</template>
+<style scaped>
+h2 {
+    color: blueviolet;
+    margin-bottom: 5px;
+    margin-top: 0px;
+}
+
+p {
+    margin-bottom: 2px;
+    margin-top: 0;
+}
+
+
+
+.caixa {
+    width: auto;
+    margin: 15px 10px;
+    padding: 10px;
+    border: 1px solid brown;
+}
+</style>
